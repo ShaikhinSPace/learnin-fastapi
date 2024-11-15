6bfe29824400311f3ec7af21a521169a2f8e3f70
sameershaikh <gitshaikh@gmail.com>
2024-11-15 16:56:24 +0545

    added bool filter and index

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from enum import Enum
+from fastapi import HTTPException
 app = FastAPI()
 
 class Category(str,Enum):
@@ -20,5 +21,19 @@ todos = {0: ToDo(title='test1',category=Category.WORK,completed='true',id='0'),
 @app.get("/")
 def index() -> dict:
     return {'todos':todos}
+
+## Another way
 #def index() -> dict[str, dict[int,ToDo]]:
 #    return {'todos':todos}
+
+@app.get('/todos/{todo_id}')
+def get_todo_by_id(todo_id:int) -> ToDo:
+    if todo_id not in todos:
+        raise HTTPException(status_code=404,detail=f"{todo_id} does not exist")
+    return todos[todo_id]
+
+@app.get('/todos/')
+def query_todo_by_completed(completed: bool | None=None) -> dict[str,list[ToDo]]:
+    filtered_todos = [todo for todo in todos.values() if todo.completed is completed]
+    return {'todos': filtered_todos}
+    
